910b1c3c08229e81eb2e08597055c472300cf6cc
松永菜月 <dicaiyue856@gmail.com>
2025-12-13 09:59:50 +0900

    マイページの取引中商品確認機能の実装

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -22,6 +22,11 @@
         <li class="sell__item"><a href="/mypage?page=sell">出品した商品</a></li>
         <li class="buy__item"><a href="/mypage?page=buy">購入した商品</a></li>
         <li class="trade__item"><a href="/mypage?page=trade">取引中の商品</a></li>
+        <li class="trade__item">
+            <a href="/mypage?page=trade">
+                取引中の商品 ({{ $items->count() }})
+            </a>
+        </li>
     </div>
     <div class="silver-line">
 
