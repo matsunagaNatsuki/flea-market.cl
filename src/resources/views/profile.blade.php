576b2b1e0d0ced5fce0ebe497e56c27cbf3fbdee
松永菜月 <dicaiyue856@gmail.com>
2025-12-16 11:36:37 +0900

    マイページ画面のcssの作成

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -14,13 +14,13 @@
         </div>
         <h2 class="profile-name">{{ optional($profile)->name }}</h2>
         @if(!is_null($reviewAvg))
-            <div class="profile-rating">
-                <span class="profile-rating__stars" aria-label="評価 {{ $reviewAvg }} / 5">
-                    @for($i=1; $i<=5; $i++)
-                        <span class="{{ $i <= $reviewAvg ? 'is-on' : '' }}">★</span>
-                    @endfor
-                </span>
-            </div>
+        <div class="profile-rating">
+            <span class="profile-rating__stars" aria-label="評価 {{ $reviewAvg }} / 5">
+                @for($i=1; $i<=5; $i++)
+                    <span class="{{ $i <= $reviewAvg ? 'is-on' : '' }}">★</span>
+            @endfor
+            </span>
+        </div>
         @endif
 
         <div class="btn">
@@ -28,61 +28,67 @@
         </div>
     </div>
 
-    <div class="tab__list">
-        <li class="sell__item"><a href="/mypage?page=sell">出品した商品</a></li>
-        <li class="buy__item"><a href="/mypage?page=buy">購入した商品</a></li>
-        <li class="trade__item">
-            <a href="/mypage?page=trade">
-                取引中の商品 ({{ $tradeMessageCount }})
-            </a>
-        </li>
+    <div class="border">
+        <ul class="tab__list">
+            <li><a href="/mypage?page=sell">出品した商品</a></li>
+            <li><a href="/mypage?page=buy">購入した商品</a></li>
+            <li><a href="/mypage?page=trade">
+                    取引中の商品 <span class="count-badge">{{ $tradeMessageCount }}</span>
+                </a>
+            </li>
+        </ul>
     </div>
-    <div class="silver-line">
 
         {{-- 出品タブ --}}
         @if ($page === 'sell')
-        @foreach ($items as $sell)
-        <div class="item">
-            <a href="/item/{{ $sell->id }}">
-                <div class="item__img--container">
-                    <img src="{{ $sell->image }}" class="item__img" alt="商品画像">
-                </div>
-                <p class="item__name">{{ $sell->name }}</p>
-            </a>
+        <div class="items">
+            @foreach ($items as $sell)
+            <div class="item">
+                <a href="/item/{{ $sell->id }}">
+                    <div class="item__img--container">
+                        <img src="{{ $sell->image }}" class="item__img" alt="商品画像">
+                    </div>
+                    <p class="item__name">{{ $sell->name }}</p>
+                </a>
+            </div>
+            @endforeach
         </div>
-        @endforeach
         @endif
 
 
         {{-- 購入タブ --}}
         @if ($page === 'buy')
-        @foreach ($items as $buy)
-        <div class="item">
-            <a href="/item/{{ $buy->sell->id }}">
-                <div class="item__img--container">
-                    <img src="{{ $buy->sell->image }}" class="item__img" alt="商品画像">
-                </div>
-                <p class="item__name">{{ $buy->sell->name }}</p>
-            </a>
+        <div class="items">
+            @foreach ($items as $buy)
+            <div class="item">
+                <a href="/item/{{ $buy->sell->id }}">
+                    <div class="item__img--container">
+                        <img src="{{ $buy->sell->image }}" class="item__img" alt="商品画像">
+                    </div>
+                    <p class="item__name">{{ $buy->sell->name }}</p>
+                </a>
+            </div>
+            @endforeach
         </div>
-        @endforeach
         @endif
 
         {{-- 取引タブ --}}
         @if ($page === 'trade')
-        @foreach ($items as $trade)
-        <div class="item">
-            <a href="{{ route('get.seller', $trade->id) }}">
-                <div class="item__img--container">
-                    <img id= item__badge src="{{ $trade->sell->image }}" class="item__img" alt="商品画像">
-                    @if(($trade->unread_count ?? 0) > 0)
+        <div class="items">
+            @foreach ($items as $trade)
+            <div class="item">
+                <a href="{{ route('get.seller', $trade->id) }}">
+                    <div class="item__img--container">
+                        <img id=item__badge src="{{ $trade->sell->image }}" class="item__img" alt="商品画像">
+                        @if(($trade->unread_count ?? 0) > 0)
                         <span class="item__badge">{{ $trade->unread_count }}</span>
-                    @endif
-                </div>
-                <p class="item__name">{{ $trade->sell->name }}</p>
-            </a>
+                        @endif
+                    </div>
+                    <p class="item__name">{{ $trade->sell->name }}</p>
+                </a>
+            </div>
+            @endforeach
         </div>
-        @endforeach
         @endif
 
     </div>
